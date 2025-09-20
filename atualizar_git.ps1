c8f97bb49afbbcbfb340c5f293abb64be1d532c0
Fabriciocypreste <fabriciocypreste@gmail.com>
2025-09-20 20:03:41 -0300

    AtualizaÃ§Ã£o automÃ¡tica

---
+++ atualizar_git.ps1
@@ -0,0 +1,6 @@
+# atualizar_git.ps1
+Set-Location "C:\Users\Fabricio\Downloads\Nova pasta (102)\CreativeHub"
+git add .
+git commit -m "Atualização automática"
+git push
+Read-Host "Pressione Enter para sair"
